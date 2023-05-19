f5a95aa98d42d33bbf74b4d47dcb24ac09e78bde
Rohan <rohandute1@gmail.com>
2023-05-19 21:02:08 +0530

    [Rohan] Created basic C-sharp project

---
+++ BasicCsharpPrograms/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCsharpPrograms
+{
+    internal class Calculator
+    {
+        public static void AddTwoNumbers()
+        {
+            Console.WriteLine("Welcome to Basic C# programs");
+            Console.WriteLine("Please enter 1st number:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter 2nd number:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int sum = x + y;
+            Console.WriteLine("Sum is:{0}", sum);
+        }
+        public static void Substract()
+        {
+            Console.WriteLine("Please enter 1st number:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter 2nd number:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int sub = x - y;
+            Console.WriteLine("Substraction is:{0}", sub);
+        }
+    }
+}
---
+++ BasicCsharpPrograms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCsharpPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Calculator.AddTwoNumbers();
+            Calculator.Substract();
+            Console.ReadLine();
+        }
+    }
+}
